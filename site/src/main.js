2d39db5e88c86b90d8c23ae1589f67ec934da439
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-08-29 23:21:28 +0200

    fixed WIP fallback

--- site/src/main.js
+++ site/src/main.js
@@ -4,25 +4,41 @@ import App from "./App.vue";
 import { routes, WIPPath } from "./router";
 import { themeStore } from "./stores/themeStore";
 
-export const createApp = ViteSSG(App, { routes }, ({ app, router }) => {
-    app.use(router);
+export const createApp = ViteSSG(
+    App,
+    {
+        routes,
+        scrollBehavior(to, from, savedPosition) {
+            if (to.hash) {
+                return { el: to.hash };
+            }
+            if (savedPosition) {
+                return savedPosition;
+            }
+            return { top: 0 };
+        },
+    },
+    ({ app, router }) => {
+        app.use(router);
+        app.config.globalProperties.$darktheme = false;
 
-    app.config.globalProperties.$darktheme = false;
-
-    if (!import.meta.env.SSR) {
-        themeStore.updateTheme();
-    }
-
-    router.beforeEach((to, from, next) => {
         if (!import.meta.env.SSR) {
-            window.scrollTo({ top: 0 });
-            document.title = `NMT | ${to.name}`;
+            themeStore.updateTheme();
         }
 
-        if (WIPPath.includes(to.path)) {
-            next({ path: "/WIP" });
-        } else {
-            next();
-        }
-    });
-});
+        router.beforeEach((to, from, next) => {
+            if (!import.meta.env.SSR) {
+                if (!to.hash) {
+                    window.scrollTo({ top: 0 });
+                }
+                document.title = `NMT | ${to.name}`;
+            }
+
+            if (WIPPath.includes(to.path)) {
+                next({ path: "/WIP" });
+            } else {
+                next();
+            }
+        });
+    },
+);
